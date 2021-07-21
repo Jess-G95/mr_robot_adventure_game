cb87522b4286e7307b0e09b60a7a05a99afe3f72
Jess-G95 <79566501+Jess-G95@users.noreply.github.com>
2021-07-21 11:06:28 +0100

    plan for game

---
+++ text_based_adventure.py
@@ -0,0 +1,28 @@
+'''
+mr robot adventure game (eXit)
+'''
+
+
+
+# trapped in a dungeon with friend. See a barrel. What do you do?
+# a. move the barrel
+# b. sit next to friend
+
+# move the barrel. find a tunnel. do you enter?
+# a. yes
+# b. no
+
+# enter the tunnel. your friend passes you a note. do you try to read the note or continue?
+# a. try to read the note
+# b. continue
+
+# try to read the note but it's too dark. do you go back or conintue?
+# a. go back
+# b. continue
+
+# you carry on and find a beach at the end of the tunnel. there is a rescue boat waiting for you. it's
+# light enough to read the note. do you read the note or get on the boat?
+# a. get on boat
+# b. read note
+
+# get on boat. you're free!!
